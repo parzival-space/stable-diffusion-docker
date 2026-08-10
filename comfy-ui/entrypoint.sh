0680adaa2ca2b9c65093a2be3e16743d2ac8b7a6
Malte Linke <me@parzival.space>
2026-08-10 02:03:33 +0200

    Update Dockerfile for compatibility with CUDA 13.3, simplify dependencies, and switch to Miniconda-based Python environment

--- comfy-ui/entrypoint.sh
+++ comfy-ui/entrypoint.sh
@@ -1,76 +1,78 @@
 #!/bin/bash
 set -e
 
-dataDir=/data
-comfyDir=$dataDir/comfy-ui
-comfyRepo="https://github.com/comfyanonymous/ComfyUI"
-subDirs=("custom_nodes" "models" "output" "venv")
+COMFY_INSTALL_DIR="/comfy-ui"
+COMFY_VENV_DIR="${COMFY_INSTALL_DIR}/.venv/comfy"
+COMFY_PYTHON_VERSION="3.13"
+COMFY_REPOSITORY="https://github.com/Comfy-Org/ComfyUI.git"
+COMFY_TAG=${COMFY_TAG:="v0.31.1"}
 
-# fix permissions (because docker mounts suck)
-sudo chown "$(whoami):$(whoami)" -R $dataDir
+MINICONDA_INSTALL_DIR="${COMFY_INSTALL_DIR}/.venv/miniconda"
+MINICONDA_INSTALL_URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+MINICONDA_ACTIVATE="${MINICONDA_INSTALL_DIR}/bin/activate"
 
-# download comfyUI if required
-if [ "$(ls -A $comfyDir)" = "" ]; then
-  echo "Downloading comfyUI..."
-  mkdir -p "$comfyDir"
-  git clone --single-branch "$comfyRepo" "$comfyDir/."
+UPDATE_DEPENDENCIES=${UPDATE_DEPENDENCIES:=false}
 
-  # create defaults
-  for subDir in "${subDirs[@]}"
-  do
-    if [ -d "$comfyDir/$subDir" ]; then
-      mv "$comfyDir/$subDir" "$comfyDir/$subDir-default"
-    fi
-  done
+# Install ComfyUI
+if [ ! -e "${COMFY_INSTALL_DIR}/.git" ]; then
+  echo "Downloading ComfyUI"
+  set -x
+  # shellcheck disable=SC2115
+  rm -rf "${COMFY_INSTALL_DIR}"/* "${COMFY_INSTALL_DIR}"/.[!.]* "${COMFY_INSTALL_DIR}"/..?* 2>/dev/null || true
+  git clone --depth 1 --branch "${COMFY_TAG}" "${COMFY_REPOSITORY}" "${COMFY_INSTALL_DIR}"
+  set +x
 fi
 
-# prepare sub dirs
-for subDir in "${subDirs[@]}"
-do
-  echo "Preparing $dataDir/$subDir..."
+# install and activate miniconda
+if [ ! -e "${MINICONDA_ACTIVATE}" ]; then
+  echo "Installing Miniconda"
+  set -x
 
-  # create dir if not existing
-  if [ ! -d "$dataDir/$subDir" ]; then
-    echo " - Creating directory..."
-    mkdir "$dataDir/$subDir"
-  fi
+  MINICONDA_INSTALLER="/tmp/Miniconda3-latest-Linux-x86_64.sh"
+  curl -o "${MINICONDA_INSTALLER}" "${MINICONDA_INSTALL_URL}"
+  bash "${MINICONDA_INSTALLER}" -b -u -p "${MINICONDA_INSTALL_DIR}"
 
-  # link to data dir
-  echo " - Updating symlink..."
-  rm -rf $comfyDir/$subDir
-  ln -s $dataDir/$subDir $comfyDir/$subDir
-
-  # prepare dir if empty
-  if [ "$(ls -A $dataDir/$subDir)" = "" ]; then
-    # copy default files if possible
-    if [ -d $comfyDir/$subDir-default ]; then
-      echo " - Copy default files..."
-      cp -R $comfyDir/$subDir-default/* $dataDir/$subDir/
-    fi
-
-    # initialize python venv
-    if [ "$subDir" = "venv" ]; then
-      echo " - Setup Python VENV..."
-      python3 -m venv $dataDir/$subDir
-    fi
-  fi
-done
-
-source $dataDir/venv/bin/activate
-cd "$comfyDir"
-
-# Install comfyUI
-if [ ! -e "$comfyDir/.comfy-setup-done" ]; then
-  echo "Installing comfyUI..."
-
-  echo " - Activate VENV and install requirements..."
-  pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
-  pip install -r "$comfyDir/requirements.txt"
-
-  echo " - Done."
-  touch "$comfyDir/.comfy-setup-done"
+  set +x
 fi
 
-LAUNCH_ARGS="${LAUNCH_ARGS:-"--preview-method auto"}"
-echo "+ python3 main.py $LAUNCH_ARGS --listen --port 8188"
-python3 $comfyDir/main.py $LAUNCH_ARGS --listen --port 8188
+echo "Activating Miniconda"
+set -x
+export CONDA_PLUGINS_AUTO_ACCEPT_TOS=${CONDA_PLUGINS_AUTO_ACCEPT_TOS:=true}
+# shellcheck disable=SC1090
+source "${MINICONDA_ACTIVATE}"
+conda init --user -q
+# shellcheck disable=SC1090
+source ~/.bashrc
+set +x
+
+# create ComfyUI virtual environment
+if [ ! -e "${COMFY_VENV_DIR}/bin/python" ]; then
+  echo "Creating ComfyUI environment"
+  set -x
+  conda create -p "${COMFY_VENV_DIR}" -y -q python=${COMFY_PYTHON_VERSION}
+  UPDATE_DEPENDENCIES=true
+  set +x
+fi
+
+echo "Activating ConfyUI environment"
+set -x
+conda activate /comfy-ui/.venv/comfy
+set +x
+
+# Installing dependencies
+if [ "$UPDATE_DEPENDENCIES" == "true" ]; then
+  echo "Installing dependencies"
+  set -x
+  # install cuda dependencies
+  pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu130
+
+  # install comfyui dependencies
+  pip install -r requirements.txt
+  set +x
+fi
+
+# Launch ComfyUI
+set -x
+# shellcheck disable=SC2086
+python main.py --listen 0.0.0.0 --port 8188 ${ARGS:=""}
+set +x
